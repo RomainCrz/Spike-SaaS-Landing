94b7586247eeb97cb01e048b2ab32e69e3989e1f
RomainCrz <101673637+RomainCrz@users.noreply.github.com>
2024-05-20 00:35:56 +0200

    logo part

--- src/components/LogoTitle.tsx
+++ src/components/LogoTitle.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export const LogoTitle = () => {
   return (
-    <div id="logo title" className="flex flex-col items-center justify-center gap-5 px-20">
+    <div id="logo title" className="flex flex-col items-center justify-center gap-10 py-14">
       <p>Trusted by teams at over 1,000 of the world's leading organizations</p>
-      <div id="logos" className="flex items-center justify-center gap-5">
+      <div id="logos" className="flex items-center justify-center gap-10">
         <Image src="/logo-dell.png" alt="dell" width={62} height={20} />
         <Image src="/logo-zendesk.png" alt="dell" width={87} height={20} />
         <Image src="/logo-rakuten.png" alt="dell" width={67} height={20} />
